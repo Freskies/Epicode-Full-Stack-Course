52cc1fbdd0af68462b9a13b05c2ba397da4d49c6
Freskies <43386854+Freskies@users.noreply.github.com>
2024-10-10 13:14:40 +0200

    practice functions

---
+++ U1-W2-D4/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// STRINGS
+
+// template literals (i've succesfully binded altgr + rshift + ' = `)
+const tl = `hello my name is ${"Valerio"} and i'm using template literals!`;
+
+const multiLineString = `this
+is
+a 
+multiline
+string`;
+
+// FUNCTIONS
+
+// funtion declaration
+function calcAge1(birthYear) {
+	return 2024 - birthYear;
+}
+
+// function expression
+const calcAge2 = function (birthYear) {
+	return 2024 - birthYear;
+};
+
+// arrow function
+const calcAge3 = birthYear => 2024 - birthYear;
+
+console.log(calcAge1(2003));
+console.log(calcAge2(2003));
