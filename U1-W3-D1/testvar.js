5e4e19e1ad97fb142d2b58997dfc0d39982524a2
Freskies <43386854+Freskies@users.noreply.github.com>
2024-10-15 11:04:42 +0200

    test var behaviors

---
+++ U1-W3-D1/testvar.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var artiglio = "collo";
+
+function lemiepalle(winObj) {
+	var accanna = "ci godo";
+	console.log(window);
+	console.log(winObj);
+}
+
+console.log(this);
+lemiepalle(this);
