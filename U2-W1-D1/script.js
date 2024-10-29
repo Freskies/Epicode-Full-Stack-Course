86c2291a772c380504c0b055686e99358c6eee6f
Freskies <43386854+Freskies@users.noreply.github.com>
2024-10-29 13:00:16 +0100

    display account data

--- U2-W1-D1/script.js
+++ U2-W1-D1/script.js
@@ -47,6 +47,7 @@ const labelTimer = document.querySelector(".timer");
 const containerApp = document.querySelector(".app");
 const containerMovements = document.querySelector(".movements");
 
+const loginForm = document.querySelector(".login");
 const btnLogin = document.querySelector(".login__btn");
 const btnTransfer = document.querySelector(".form__btn--transfer");
 const btnLoan = document.querySelector(".form__btn--loan");
@@ -60,3 +61,108 @@ const inputTransferAmount = document.querySelector(".form__input--amount");
 const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
+
+// global variables
+let currentAccount = undefined;
+
+// USER
+const createUsernames = users => {
+	const createUsername = user =>
+		user
+			.split(" ")
+			.map(partName => partName[0])
+			.join("")
+			.toLowerCase();
+
+	users.forEach(user => (user.username = createUsername(user.owner)));
+};
+
+createUsernames(accounts);
+
+// MOVEMENTS
+const displayMovements = movements => {
+	containerMovements.innerHTML = "";
+
+	movements.forEach((movement, i) => {
+		const type = movement > 0 ? "deposit" : "withdrawal";
+		const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+			i + 1
+		} ${type}</div>
+        <div class="movements__value">${movement}€</div>
+      </div>
+    `;
+		containerMovements.insertAdjacentHTML("afterbegin", html);
+	});
+};
+
+// BALANCE
+const calcBalance = movements =>
+	movements.reduce((acc, movement) => (acc += movement), 0);
+
+const displayBalance = balance => (labelBalance.textContent = `${balance} EUR`);
+
+const updateBalance = user => {
+	displayBalance(calcBalance(user.movements));
+};
+
+const calcIncomingSummary = movements =>
+	movements
+		.filter(movement => movement > 0)
+		.reduce((acc, movement) => acc + movement, 0);
+
+const calcOutcomingSummary = movements =>
+	movements
+		.filter(movement => movement < 0)
+		.reduce((acc, movement) => acc + movement, 0);
+
+const calcInterestSummary = (movements, intrestRate) =>
+	movements
+		.filter(movement => movement > 0)
+		.map(deposit => (deposit * intrestRate) / 100)
+		.filter(interest => interest >= 1)
+		.reduce((acc, interest) => acc + interest, 0);
+
+const displaySummary = (incoming, outcoming, interest) => {
+	labelSumIn.textContent = `${incoming} €`;
+	labelSumOut.textContent = `${outcoming} €`;
+	labelSumInterest.textContent = `${interest} €`;
+};
+
+const updateSummary = ({ movements, interestRate }) => {
+	displaySummary(
+		calcIncomingSummary(movements),
+		calcOutcomingSummary(movements),
+		calcInterestSummary(movements, interestRate),
+	);
+};
+
+// LOGIN
+
+const login = account => {
+	// display UI
+	containerApp.style.opacity = 100;
+
+	// display welcome message
+	labelWelcome.textContent = `Welcome back, ${account.owner.split(" ")[0]}`;
+
+	// display data from account
+	displayMovements(account.movements);
+	updateBalance(account);
+	updateSummary(account);
+};
+
+loginForm.addEventListener("submit", e => {
+	e.preventDefault();
+	const inputUsername = inputLoginUsername.value;
+	const inputPin = inputLoginPin.value;
+
+	currentAccount = accounts.find(({ username }) => username === inputUsername);
+	if (currentAccount?.pin === Number(inputPin)) login(currentAccount);
+
+	// clear form
+	inputLoginUsername.value = "";
+	inputLoginPin.value = "";
+	inputLoginPin.blur();
+});
