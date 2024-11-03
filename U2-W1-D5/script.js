267e3ba6dabfb8f4a61e040dd5e6c44cb806f409
Freskies <43386854+Freskies@users.noreply.github.com>
2024-11-03 14:36:26 +0100

    animation completed

--- U2-W1-D5/script.js
+++ U2-W1-D5/script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+/////////////////////////////
+// STICKY SCROLL
+////////////////////////////
+
+const header = document.querySelector(".header");
+const hero = document.querySelector(".section-hero");
+const scrolledHeight = hero.offsetHeight - header.offsetHeight;
+
+window.addEventListener("scroll", () => {
+	if (window.scrollY > scrolledHeight) header.classList.add("scrolled");
+	else header.classList.remove("scrolled");
+});
+
+/////////////////////////////
+// M-ANIMATION
+////////////////////////////
+
+// set css variable for the "media-query" scrolling
+const title = document.querySelector(".hero-wrapper");
+const titleWidth = title.offsetWidth;
+document.documentElement.style.setProperty(
+	"--hero-wrapper-width",
+	`${titleWidth}px`,
+);
+
+// actual animation
+const allM = document.querySelectorAll(`.m-layout g[stroke-linecap="butt"]`);
+
+const animateM = () => {
+	const m = allM[Math.floor(Math.random() * allM.length)];
+	m.style.opacity = (m.style.opacity - 0 + 1) % 2;
+};
+
+setInterval(animateM, 16);
