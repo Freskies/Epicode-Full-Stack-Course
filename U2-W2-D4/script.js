d456339e76c8835b266807eb0d1907cce489ae7b
Freskies <43386854+Freskies@users.noreply.github.com>
2024-11-11 09:30:00 +0100

    clear

--- U2-W2-D4/script.js
+++ U2-W2-D4/script.js
@@ -1,14 +1,29 @@
-const mainNavigation = document.getElementById("main-navigation");
+'use strict';
 
-mainNavigation.addEventListener("click", e => {
-	console.log(e.target);
-	// if the clicked element is a fakeDropdown toggle it
-	if (e.target.classList.contains("fake-dropdown")) {
-		const dataToggle = e.target.getAttribute("data-dropdown");
-		console.log(dataToggle);
-		e.target.setAttribute(
-			"data-dropdown",
-			dataToggle === "true" ? "false" : "true",
-		);
-	}
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
 });
