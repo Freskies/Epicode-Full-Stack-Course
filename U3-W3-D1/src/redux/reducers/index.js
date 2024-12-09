b85af683fac6c42e01269f929bc6e9caafe5e15d
Freskies <43386854+Freskies@users.noreply.github.com>
2024-12-09 15:01:16 +0100

    getting started with redux

---
+++ U3-W3-D1/src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+	// Define your initial state here
+};
+
+const mainReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default mainReducer;
